0ae32482ca47589fe7202cbbf0e7ca9b88df5293
tomato23d <anna.a.art@gmail.com>
2023-04-29 14:10:56 +1000

    server file is drafted

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//GET route for landing page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET route for notes page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
+
