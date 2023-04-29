f716f433e2b3266567aee1c9dece12da60406d1f
tomato23d <anna.a.art@gmail.com>
2023-04-29 15:48:14 +1000

    get route is tested fine

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const notesData = require('./db/note.json');
 const fs = require('fs');
 const util = require('util');
+const uuid = require('./helpers/uuid');
 
 
 const PORT = 3001;
@@ -20,6 +22,23 @@ app.get('/', (req, res) =>
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+//GET route to retrieve existing notes
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+app.get('/api/notes/:type', (req, res) => {
+    
+    const requestedType = req.params.type.toLowerCase();
+    const output = [];
+  
+    for (let i = 0; i < notesData.length; i++) {
+      if (requestedType === notesData[i].type.toLowerCase()) {
+        output.push(notesData[i]);
+      }}
+      return res.json(output);
+    });
+
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
