08e456b78b418464e5df6c5a5a7c0956b5b36d97
tomato23d <119089151+tomato23d@users.noreply.github.com>
2023-05-02 23:52:55 +1000

    Update server.js

--- server.js
+++ server.js
@@ -13,13 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//GET route for landing page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 //GET route for notes page
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
@@ -78,8 +74,13 @@ app.post('/api/notes',(req,res) => {
     readAndAppend();
                         
      };})
- 
-app.listen(PORT, () =>
+
+//GET route for landing page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+ app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
